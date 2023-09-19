1f1ec8d1c8ac5be82fc8bb49e19274a37e80c148
billym711 <bmoorekid@gmail.com>
2023-09-19 15:28:23 -0400

    Transferring data from screens

--- app/src/main/java/com/example/project3/MainFragment.kt
+++ app/src/main/java/com/example/project3/MainFragment.kt
@@ -2,10 +2,15 @@ package com.example.project3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 
@@ -24,6 +29,7 @@ class MainFragment : Fragment() {
     private var multiplicationWait: Boolean = false
 
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -31,11 +37,66 @@ class MainFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_main, container, false)
         val startButton = view.findViewById<Button>(R.id.button)
+        val diffButtonEasy = view.findViewById<RadioButton>(R.id.testButton)
+        val diffButtonMedium = view.findViewById<RadioButton>(R.id.radioButton2)
+        val diffButtonHard = view.findViewById<RadioButton>(R.id.radioButton3)
+        val operButtonAdd = view.findViewById<RadioButton>(R.id.radioButton7)
+        val operButtonMult = view.findViewById<RadioButton>(R.id.radioButton8)
+        val operButtonDiv = view.findViewById<RadioButton>(R.id.radioButton9)
+        val operButtonSub = view.findViewById<RadioButton>(R.id.radioButton10)
+        var difficultyGroup = view.findViewById<RadioGroup>(R.id.difficulty)
+        var operationGroup = view.findViewById<RadioGroup>(R.id.operation)
+        val plusButton = view.findViewById<ImageButton>(R.id.imageButton)
+        val minusButton = view.findViewById<ImageButton>(R.id.imageButton2)
 
-        startButton.setOnClickListener {
-            view.findNavController()
-                .navigate(R.id.action_mainFragment_to_act23)
+        var diff = "Easy"
+        var oper = "Addition"
+        var questionNum = 10
+        difficultyGroup.setOnCheckedChangeListener { group, checkedId ->
+            if (diffButtonEasy.isChecked) {
+                diff = "Easy"
+            }
+            if (diffButtonMedium.isChecked) {
+                diff = "Medium"
+            }
+            if (diffButtonHard.isChecked) {
+                diff = "Hard"
+            }
         }
+            operationGroup.setOnCheckedChangeListener { group, checkedId ->
+            if (operButtonAdd.isChecked) {
+                oper = "Add"
+            }
+            if (operButtonMult.isChecked) {
+                oper = "Multiply"
+            }
+            if (operButtonDiv.isChecked) {
+                oper = "Divide"
+            }
+            if (operButtonSub.isChecked) {
+                oper = "Subtract"
+            }
+        }
+
+
+        var numOfQuestions = view.findViewById<TextView>(R.id.textView4)
+        numOfQuestions.text = questionNum.toString()
+
+        plusButton.setOnClickListener {
+            questionNum++
+            numOfQuestions.text = questionNum.toString()
+        }
+        minusButton.setOnClickListener {
+            questionNum--
+            numOfQuestions.text = questionNum.toString()
+        }
+        startButton.setOnClickListener {
+            val action = MainFragmentDirections.actionMainFragmentToAct23(diff, oper)
+            view.findNavController()
+                .navigate(action)
+        }
+
         return view
     }
+
 }
--- app/src/main/java/com/example/project3/endScreen.kt
+++ app/src/main/java/com/example/project3/endScreen.kt
@@ -2,17 +2,14 @@ package com.example.project3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import org.w3c.dom.Text
 
-class Act2 : Fragment() {
+class endScreen : Fragment() {
 
     // This establishes some basic variables to store numbers and convert them to a printable string
     // We also have booleans to determine whether or not we are currently doing an operation, and which specific operation we're doing.
@@ -31,20 +28,13 @@ class Act2 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.act2, container, false)
-        val startButton = view.findViewById<Button>(R.id.button2)
-        val message = Act2Args.fromBundle(requireArguments()).difficulty
-        val message2 = Act2Args.fromBundle(requireArguments()).operation
-        val operationText = view.findViewById<TextView>(R.id.textView7)
-        Log.d("test", message)
-        Log.d("test", message2)
-        operationText.text = message2
+        val view = inflater.inflate(R.layout.fragment_end_screen, container, false)
+        val startButton = view.findViewById<Button>(R.id.button3)
 
         startButton.setOnClickListener {
             view.findNavController()
-                .navigate(R.id.action_act23_to_endScreen)
+                .navigate(R.id.action_endScreen_to_mainFragment)
         }
-
         return view
     }
 }
